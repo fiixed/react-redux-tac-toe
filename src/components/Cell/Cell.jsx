2842790fd7be0c727543e281641c6a6839ea3656
fiixed <fiixed@gmail.com>
2021-01-28 11:53:30 -0500

    wrong branch

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Cell.css';
 
 export default function Cell(props) {
+    let cellContentClasses = 'cell-content';
     return (
         <button className="cell">
             <span className="cell-content populated">{props.value}</span>
