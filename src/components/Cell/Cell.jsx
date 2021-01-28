ebb1c48a533c44246230ec22033ee106b758dad0
fiixed <fiixed@gmail.com>
2021-01-28 12:01:58 -0500

    add classnames and canHighlight state

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import classNames from 'classnames';
+
 import './Cell.css';
 
 export default function Cell(props) {
+    const cellClasses = classNames({
+        cell: true,
+        winner: props.canHighlight
+    });
+    
+    const cellContentClasses = classNames({
+        'cell-content': true,
+        populated: props.value
+    });
+
+    
     return (
-        <button className="cell">
-            <span className="cell-content populated">{props.value}</span>
+        <button className={cellClasses}>
+            <span className={cellContentClasses}>{props.value}</span>
         </button>
     )
 }
