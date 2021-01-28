6a9ffcf5e76558e98627704ab62e56a3610f2278
fiixed <fiixed@gmail.com>
2021-01-28 09:50:38 -0500

    finished components

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Cell from '../Cell/Cell';
+
+import './Board.css';
+
+export default function Board() {
+    return (
+        <div id="board">
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />      
+        </div>
+        
+    )
+}
